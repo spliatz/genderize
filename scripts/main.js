4713f2b767ca4c7da9031f340b558569baa401c9
David <spliatzoff@gmail.com>
2022-03-22 08:45:02 +0400

    перевод на русский язык

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,10 @@
 'use strict'
-import {USER} from "./view.js";
+import {alphabet, USER} from "./view.js";
 import {DATA} from "./view.js";
+import {translate} from "./view.js";
 
 function getURL(api,name)  {
-    return `${api}?name=${name}`
+    return `${api}?name=${translate(name)}`
 }
 
 const response = fetch(getURL(DATA.API,USER.NAME));
@@ -11,8 +12,5 @@ const json = response.then(response => response.json());
 
 json.then(
     result => {
-    alert(`${result.name} - ${result.gender}`);
-    for (const resultKey in result) {
-        console.log(resultKey + ' ' + result[resultKey])
-    }git
+    alert(`${USER.NAME} - ${result.gender === 'male' ? "мужское имя" : "женское имя"}`);
 })
--- scripts/view.js
+++ scripts/view.js
@@ -2,8 +2,30 @@ export const USER =  {
     NAME: prompt('Name: ', ''),
 }
 
+
+
+export function translate (string) {
+    const array = string.split('');
+    array.forEach((item,index) => {
+        array[index] = alphabet[item];
+    })
+    return (array.join(''));
+}
+
 export const DATA = {
      API : 'https://api.genderize.io',
      // "url" : `${this.serverUrl}?name=${USER.NAME}`,
 }
 
+export let alphabet = {
+    А: 'A', Б: 'B', В: 'V', Г: 'G', Д: 'D', Е: 'E', Ё: 'Yo', Ж: 'Zh',
+    З: 'Z', И: 'I', Й: 'J', К: 'K', Л: 'L', М: 'M', Н: 'N', О: 'O',
+    П: 'P', Р: 'R', С: 'S', Т: 'T', У: 'U', Ф: 'F', Х: 'H', Ц: 'C',
+    Ч: 'Ch', Ш: 'Sh', Щ: 'Sh', Ъ: '', Ы: 'Y', Ь: '', Э: 'E', Ю: 'Yu',
+    Я: 'Ya',
+    а: 'a', б: 'b', в: 'v', г: 'g', д: 'd', е: 'e', ё: 'yo', ж: 'zh',
+    з: 'z', и: 'i', й: 'y', к: 'k', л: 'l', м: 'm', н: 'n', о: 'o',
+    п: 'p', р: 'r', с: 's', т: 't', у: 'u', ф: 'f', х: 'h', ц: 'ts',
+    ч: 'ch', ш: 'sh', щ: 'sh', ъ: '', ы: 'y', ь: '', э: 'e', ю: 'yu',
+    я: 'ya',
+}
