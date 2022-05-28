6390fb6fa024380838b4aa6910fb9b1e36defff8
David <spliatzoff@gmail.com>
2022-05-29 00:11:11 +0400

    nothing important

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,4 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement);
-root.render(
-        <App/>
-);
+root.render(<App/>);
