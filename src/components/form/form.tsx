f8b49210dc2033879edec8c9253edcd124188d20
David <spliatzoff@gmail.com>
2022-05-30 15:47:43 +0400

    kludge fixed and add npm package transliteration

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { translate } from './russianToEnglishTranslate';
+import { transliterate as translate } from 'transliteration';
 import './form.scss';
 
 interface Response {
